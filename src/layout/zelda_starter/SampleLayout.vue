fe0a1f26410f3a552716cf7fdcfe394463e3ff79
zhu li <cruise_lee@outlook.com>
2020-06-25 21:54:55 +0800

    add simple click event

--- src/layout/zelda_starter/SampleLayout.vue
+++ src/layout/zelda_starter/SampleLayout.vue
@@ -2,8 +2,12 @@
   <div class="wrapper">
     <side-bar>
       <template slot="links">
+        <div @click="subitem=!subitem">
         <sidebar-link to="/dashboard" :name="$t('sidebar.dashboard')" icon="tim-icons icon-chart-pie-36"/>
+        </div>
+        <div v-if="subitem==true">
         <sidebar-link v-for="product in products" :key="product" to="/profile" :name="product" />
+        </div>
         <sidebar-link to="/test-details" :name="$t('sidebar.testDetails')" icon="tim-icons icon-puzzle-10"/>
         <sidebar-link to="/profile" :name="$t('sidebar.userProfile')" icon="tim-icons icon-single-02"/>
         <sidebar-link to="/calendar" :name="$t('sidebar.calendar')" icon="tim-icons icon-calendar-60"/> 
@@ -36,7 +40,8 @@
     },
     data() {
       return {
-        products: ["podman", "docker"]
+        products: ["podman", "docker"],
+        subitem: false
       }
       
     },
@@ -45,7 +50,7 @@
         if (this.$sidebar.showSidebar) {
           this.$sidebar.displaySidebar(false);
         }
-      }
+      },
     }
   };
 </script>
